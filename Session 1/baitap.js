995e4d84ad26b860da462cb008c8863ca779cdcf
kienngo95 <ngosykien051095@gmail.com>
2021-03-12 21:41:39 +0700

    baitap1

---
+++ Session 1/baitap.js
@@ -0,0 +1,86 @@
+console.log("Ngô Sỹ Kiên");
+console.log("26");
+
+//Bài 1//
+//Q1//
+let q1 = 'Có 3 cách gây lỗi khi làm lệnh'
+let a = 'Không được viết số ở đầu câu lệnh';
+let b = 'Không được trùng với các từ khóa của JS';
+let c = 'Không được có các kí tự đặc biệt';
+console.log(q1);
+console.log(a,b,c);
+
+let vd = '3 ví dụ';
+let vd1 = '4kienkien';
+let vd2 = 'consolekiên';
+let vd3 = 'kien_dep_zai';
+console.log(vd); 
+console.log(vd1, vd2, vd3);
+
+//Q2//
+let q2 = 'Để kiểm tra 1 kiểu dữ liệu biến, ta phải:';
+console.log(q2);
+let traloi = 'Kiểm tra khai báo biến đó là không dấu, dấu nháy đơn, nháy kép hay backtick.';
+console.log(traloi);
+
+//Bài 2//
+console.log('Đáp án là: 1-b 2-a 3-e 4-c');
+
+//Bài 3//
+//a. String:
+let message = 'Coding is great';
+ console.log(message);
+
+//b. Number
+let studentCount = 0
+console.log(studentCount);
+
+//Bài 4//
+{a} let message1 = 'Coding might not be easy, but still great';
+console.log(message1);
+
+{b} let studentCount1 = 17;
+console.log(studentCount1);
+
+{c} 
+console.log(message1.toLowerCase());
+
+//d// 
+console.log(studentCount1 + 1);
+
+//Bài 5//
+let kien = 'You look beautiful today';
+alert(kien);
+
+//Bài 6//
+let yourName = prompt("Hi there, your name please?");
+console.log(yourName);
+alert(`Hi ${yourName}`);
+
+//Bài 7//
+let firstName = prompt("Enter your first name?");
+console.log(firstName);
+let lastName = prompt("Enter your last name?");
+console.log(lastName);
+alert(`Hi ${firstName +" "+ lastName}`);
+
+//Bài 8//
+let length = prompt("Hãy viết vào chiều dài cạnh hình vuông :");
+console.log(length*length);
+alert(`Diện tích hình vuông là ${length*length}`);
+
+//Bài 9//
+let r = prompt("Hãy viết vào bán kính hình tròn :");
+console.log((r*r)*3.14);
+alert(`Diện tích hình tròn là ${((r*r)*3.14)}`);
+
+//Bài 10//
+let C = prompt('Hãy viết số độ C cần chuyển sang độ F :');
+console.log((C*1.8)+32);
+alert(`${C} (C) = ${((C*1.8)+32)} (F)`);
+
+
+
+
+
+
